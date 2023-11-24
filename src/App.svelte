afb06ce1d377c4608bfdbf0529491d54777dea00
julesrx <dev@julesrx.fr>
2023-11-24 11:16:01 +0100

    frames

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,26 @@
   import f2 from './assets/frame2.png';
   import f3a from './assets/frame3a.png';
   import f3b from './assets/frame3b.png';
+
+  const frames = [
+    { id: 'f-s', src: fs, hidden: false },
+    { id: 'f-1', src: f1, hidden: true },
+    { id: 'f-2', src: f2, hidden: true },
+    { id: 'f-3a', src: f3a, hidden: true },
+    { id: 'f-3b', src: f3b, hidden: true }
+  ];
 </script>
 
 <main>
   <div id="app" class="h-screen flex justify-center items-center">
-    <img id="f-s" src={fs} alt="frame" class="frame" />
-    <img id="f-1" src={f1} alt="frame" class="frame hidden" />
-    <img id="f-2" src={f2} alt="frame" class="frame hidden" />
-    <img id="f-3a" src={f3a} alt="frame" class="frame hidden" />
-    <img id="f-3b" src={f3b} alt="frame" class="frame hidden" />
+    {#each frames as frame}
+      <img
+        id={frame.id}
+        src={frame.src}
+        alt={'frame-' + frame.id}
+        class:frame={true}
+        class:hidden={frame.hidden}
+      />
+    {/each}
   </div>
 </main>
