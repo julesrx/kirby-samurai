3d2a72f445d46115195ce19e2b0660498a17bafc
julesrx <dev@julesrx.fr>
2023-11-24 11:12:01 +0100

    frames

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,17 @@
 <script lang="ts">
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import fs from './assets/start.png';
+  import f1 from './assets/frame1.png';
+  import f2 from './assets/frame2.png';
+  import f3a from './assets/frame3a.png';
+  import f3b from './assets/frame3b.png';
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
+  <div id="app" class="h-screen flex justify-center items-center">
+    <img id="f-s" src={fs} alt="frame" class="frame" />
+    <img id="f-1" src={f1} alt="frame" class="frame hidden" />
+    <img id="f-2" src={f2} alt="frame" class="frame hidden" />
+    <img id="f-3a" src={f3a} alt="frame" class="frame hidden" />
+    <img id="f-3b" src={f3b} alt="frame" class="frame hidden" />
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import './app.css';
 import App from './App.svelte';
 
-const app = new App({
-  target: document.getElementById('app')
-});
+const app = new App({ target: document.getElementById('app') });
 
 export default app;
 
